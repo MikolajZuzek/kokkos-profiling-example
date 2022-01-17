cdeaec1c0f7052c33e0351c85afcf0e7d6b9ccb8
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2022-01-17 15:46:43 +0100

    Move the computation to separate header

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <Kokkos_Core.hpp>
 #include "kp_all.hpp"
-
-template <typename data_type>
-extern int run_calculation(const data_type SIZE); // defined below
+#include "kernels.hpp"
 
 //-------------------------------------------------------------------------------------//
 
@@ -28,34 +26,3 @@ int main(int argc, char *argv[])
 }
 
 //-------------------------------------------------------------------------------------//
-
-// Sample computation: S(N) = 1 + 2 + 3 + ... + N
-// Tests: regions, allocation, parallel for and reduction
-template <typename data_type>
-int run_calculation(const data_type SIZE)
-{
-  Kokkos::Profiling::pushRegion("Computation");
-
-  Kokkos::View<data_type*> data(Kokkos::ViewAllocateWithoutInitializing("data"), SIZE);
-  Kokkos::parallel_for("initialize()", SIZE, KOKKOS_LAMBDA(data_type i) {
-    data(i) = i;
-  });
-  Kokkos::fence();
-
-  data_type sum = 0;
-  Kokkos::parallel_reduce("accumulate()", SIZE, KOKKOS_LAMBDA(data_type i, data_type &lsum) {
-    lsum += 1 + data(i);
-  }, sum);
-  Kokkos::fence();
-
-  Kokkos::Profiling::popRegion();
-
-  // check results
-  const data_type check = (SIZE + 1) * SIZE / 2;
-  if (sum != check) {
-    std::cout << "BAD result, got S(" << SIZE << ") = " << sum << " (expected " << check << ")" << std::endl;
-    return 1;
-  }
-  std::cout << "Result OK: S(" << SIZE << ") = " << sum << std::endl;
-  return 0;
-}
